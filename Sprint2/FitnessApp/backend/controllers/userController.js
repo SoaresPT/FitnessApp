33899736a59952b8624b294b353eee738bae8c6a
Soares <soares_123@msn.com>
2023-09-21 05:30:34 +0300

    Expand signup page to detect already existing users. Added message showing sign-up results

--- Sprint2/FitnessApp/backend/controllers/userController.js
+++ Sprint2/FitnessApp/backend/controllers/userController.js
@@ -18,9 +18,18 @@ async function signup(req, res) {
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+
+    // Check if the error is a MongoDB duplicate key error (error code 11000)
+    if (error.code === 11000) {
+      // Respond with a custom error message for duplicate email
+      res.status(400).json({ error: 'Signup failed. This user already has an account.' });
+    } else {
+      // Handle any other errors and respond with a generic error message
+      res.status(500).json({ error: 'Internal server error' });
+    }
   }
 }
+
 
 // Function to get all registered users
 async function getAllUsers(req, res) {
--- Sprint2/FitnessApp/frontend/src/pages/Signup.js
+++ Sprint2/FitnessApp/frontend/src/pages/Signup.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signupStatus, setSignupStatus] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,39 +19,51 @@ const Signup = () => {
       });
 
       if (response.ok) {
-        // Request was successful
         const data = await response.json();
         console.log("Signup successful!", data);
+        setSignupStatus("Signup successful!");
+        setEmail("");
+        setPassword("");
       } else {
-        // Request failed
         const errorData = await response.json();
         console.error("Signup failed:", errorData);
+        setSignupStatus(errorData.error); // Set the error message received from the backend
       }
     } catch (error) {
-      // Handle network or other errors
       console.error("Error:", error);
+      setSignupStatus("An error occurred. Please try again later.");
     }
   };
 
   return (
-    <form className="signup" onSubmit={handleSubmit}>
-      <h3>Sign Up</h3>
-      
-      <label>Email address:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
-      />
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
-      />
+    <div>
+      <form className="signup" onSubmit={handleSubmit}>
+        <h3>Sign Up</h3>
 
-      <button>Sign up</button>
-    </form>
+        <label>Email address:</label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+
+        <button>Sign up</button>
+      </form>
+
+      {signupStatus && <p>{signupStatus}</p>}
+
+      {signupStatus === "Signup successful!" && (
+        <p>
+          <Link to="/login">Login here</Link>
+        </p>
+      )}
+    </div>
   );
 };
 
