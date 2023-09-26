d670cd6caa4dba44ce85ef92c24138e2bb46b8c8
Soares <soares_123@msn.com>
2023-09-26 17:32:28 +0300

    Explore page

--- Sprint3/FitnessApp/frontend/src/pages/Explore.js
+++ Sprint3/FitnessApp/frontend/src/pages/Explore.js
@@ -1,8 +1,12 @@
 import React from "react";
-
+import ExerciseMap from "../components/Exercisemap"
 const Explore = () => {
     return (
+        <div>
+            
         <div>Explore some exercises</div>
+        <ExerciseMap />
+        </div>
     )
 }
 
