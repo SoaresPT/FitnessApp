832310a804c95cb4a7cd5fa9aab03a625b6c39fc
andreydeshko <andydeshko@gmail.com>
2023-10-05 00:57:12 +0300

    empty fields must be fixed or deleted

--- Sprint3/FitnessApp/backend/controllers/workoutController.js
+++ Sprint3/FitnessApp/backend/controllers/workoutController.js
@@ -31,7 +31,7 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body
 
-  let emptyFields = [];
+  let emptyFields = []
 
   if (!title) {
     emptyFields.push('title')
@@ -43,9 +43,8 @@ const createWorkout = async (req, res) => {
     emptyFields.push('reps')
   }
   if (emptyFields.length > 0) {
-    return res.status(400).json({error: 'Fill all the fields to save workout', emptyFields})
+    return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
   }
-
 
 
 
